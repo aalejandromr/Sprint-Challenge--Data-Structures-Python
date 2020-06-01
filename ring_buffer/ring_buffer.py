504095bcb4d0c63b61e2123cdd9208128024cc37
Alejandro Mejia <aalejandromr@AlejandrosAir2.lan1>
2020-05-31 21:59:52 -0400

    Gets all exercises done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
+import sys
+sys.path.append('../../Data-Structures/doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.attemps = 0
+        self.storage = [None]*capacity
+        self.current = None
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.attemps == self.capacity:
+            self.attemps = 0
+            self.storage[self.attemps] = item
+        else:
+            self.storage[self.attemps] = item
+        self.attemps += 1
 
     def get(self):
-        pass
+        return [_ for _ in self.storage if _]
